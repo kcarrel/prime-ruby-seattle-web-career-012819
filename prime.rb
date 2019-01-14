13cd6ce8ce3b5b997153ac9d905c54cc41c1d576
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-14 21:48:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1 
+    FALSE
+  elsif integer >= 2 
+    (2..integer).each do |divisor|
+      if integer % divisor == 0  
+        FALSE
+      end
+    end
+  end
+  TRUE
+end
